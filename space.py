6dea02b7d26b81affa53f62ce9413251e1b6dc43
Glerious <jonascouturon2@gmail.com>
2025-04-29 13:37:16 +0200

    convert element from planete to MorPlanete

--- space.py
+++ space.py
@@ -7,7 +7,6 @@ SUN_MASS = 1.988e30
 
 class Planete:
     def __init__(self, ray_: int, influence_: int,
-                 density_: float, atmospheric_height_,
                  to_sun_: int = 0, speed_: float = 0,
                  sun_mass_compare_: float = 1):
         self.mass_compare: float = sun_mass_compare_
@@ -15,8 +14,6 @@ class Planete:
         self.to_sun: int = to_sun_
         self.spherical_influence: int = influence_
         self.speed: int = speed_
-        self.ground_density: float = density_
-        self.atmospheric_height: int = atmospheric_height_
 
     def get_revolution(self) -> float:
         return 4*pi**2*self.to_sun**3/K**2
@@ -38,6 +35,14 @@ class Planete:
     
     def get_hight_gravity(self, height_: float):
         return self.get_gravity()*(self.ray/(self.ray + height_))**2
+    
+class MorePlanete(Planete):
+    def __init__(self, ray_, influence_,
+                 density_: float, atmospheric_height_,
+                 to_sun_ = 0, speed_ = 0, sun_mass_compare_ = 1):
+        super().__init__(ray_, influence_, to_sun_, speed_, sun_mass_compare_)
+        self.ground_density: float = density_
+        self.atmospheric_height: int = atmospheric_height_
 
 class Module:
     def __init__(self, mass_: float, drag_coefficient_: float, area_: float):
@@ -49,6 +54,5 @@ class Module:
         return self.mass/(self.drag_coefficient*self.area)
     
 SUN = Planete(ray_=6.957e8, influence_=15e9)
-VENUS = Planete()
 EARTH = Planete(ray_=6378, influence_=9e5, to_sun_=150e6, speed_=29.78, sun_mass_compare_=1/332_946.0487)
 JUPITER = Planete(ray_=69911, influence_=48.2e6, to_sun_=5.2026, speed_= 13.0585, sun_mass_compare_=1/1047.348625)
